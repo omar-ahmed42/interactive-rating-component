1b7a739cd61117ad9ad0ea099b061fd19c6a2ed1
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-04-26 16:05:38 +0300

    Add thank you card after submission

--- rating.js
+++ rating.js
@@ -25,6 +25,24 @@ let ratingSubmitBtn = document.getElementsByClassName('submit-btn rating-btn');
 
 ratingSubmitBtn[0].addEventListener('click', () => {
   console.log('submit btn triggered');
-  if (selectedRating) return selectedRating;
+  if (selectedRating) {
+    let card = document.getElementsByClassName('card')[0];
+    card.classList.replace('card', 'thanks-card');
+    card.innerHTML = `
+    <div class="card-content-wrapper">
+        <div class="submission-wrapper">
+            <img src="assets/images/illustration-thank-you.svg" alt="Thank you">
+        </div>
+        <div class="selection-wrapper">
+            <div class="selection-result">You selected ${selectedRating} out of 5</div>
+        </div>
+        <div class="thank-header">Thank you!</div>
+        <div class="thank-description">We appreciate you taking the time to give a rating. If you ever need more support, 
+            don’t hesitate to get in touch!</div>
+        </div>
+      `;
+    return selectedRating;
+  }
+
   alert('You must select a rating before submitting');
 });
